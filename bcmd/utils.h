8ea696acd999162395208620070efc087acf86fb
Felipe Tavoni <56005905+FelTavoni@users.noreply.github.com>
2024-05-06 22:57:04 -0300

    [Exerc 2] Exported bcmd exercise

---
+++ bcmd/utils.h
@@ -0,0 +1,17 @@
+/*
+ *    SPDX-FileCopyrightText: 2024 Felipe Tavoni <56005905+FelTavoni@users.noreply.github.com>
+ *    SPDX-FileCopyrightText: 2024 Monaco F. J. <monaco@usp.br>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  Felipe Tavoni <56005905+FelTavoni@users.noreply.github.com>
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+int __attribute__((fastcall)) strcmp(const char *s1, const char *s2);
+
+#endif  /* UTILS_H  */
