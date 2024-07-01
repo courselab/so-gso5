dcf4ca740e1c05213c26fb23ce3def3e13657c24
Felipe Tavoni <56005905+FelTavoni@users.noreply.github.com>
2024-07-01 19:46:16 -0300

    [Exerc 3] Exported Exercise 3

---
+++ mydos/prog.c
@@ -0,0 +1,17 @@
+/*
+ *    SPDX-FileCopyrightText: 2024 Felipe Tavoni <56005905+FelTavoni@users.noreply.github.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  Felipe Tavoni <56005905+FelTavoni@users.noreply.github.com>
+ */
+
+#include "tydos.h"
+
+int main()
+{
+  puts ("Hello World\n");
+  return 0;
+}
